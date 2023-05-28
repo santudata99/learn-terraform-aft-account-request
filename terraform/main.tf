4c988a371dc6e79ac18dea4963c0564c612d6380
santudata99 <santudata9@gmail.com>
2023-05-28 03:20:58 -0700

    add account thevjajer-IBG-PROD

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,13 @@
-module "sandbox" {
+module "thevjajer-IBG-PROD" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "santudata9@gmail.com"
+    AccountName               = "thevjajer-IBG-PROD"
+    ManagedOrganizationalUnit = "ITSS-PROD"
+    SSOUserEmail              = "santudata9@gmail.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
   }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
+  ...
 }
