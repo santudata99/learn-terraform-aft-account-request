e80b034686db455191265057d73ec55c7a2e791e
santudata99 <santudata9@gmail.com>
2023-05-28 20:47:05 -0700

    sandbox-update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,27 @@
-module "thevjajer-IBG-PROD" {
+module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "santudata9@gmail.com"
-    AccountName               = "thevjajer-IBG-PROD"
-    ManagedOrganizationalUnit = "ITSS-PROD"
-    SSOUserEmail              = "santudata9@gmail.com"
-    SSOUserFirstName          = "Admin"
-    SSOUserLastName           = "User"
+    AccountEmail              = "santusan043@gmail.com"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "santusan043@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
   }
-  ...
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
 }
