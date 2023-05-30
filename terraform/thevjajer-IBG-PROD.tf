a1681d9499550edc40aa2f01d7bb931c42252290
santudata99 <santudata9@gmail.com>
2023-05-29 22:26:17 -0700

    adding existing account

---
+++ terraform/thevjajer-IBG-PROD.tf
@@ -0,0 +1,27 @@
+module "thevjajer-IBG-PROD" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "santudata9@gmail.com"
+    AccountName               = "thevjajer-IBG-PROD"
+    ManagedOrganizationalUnit = "ITSS-PROD"
+    SSOUserEmail              = "santudata9@gmail.com"
+    SSOUserFirstName          = "santu"
+    SSOUserLastName           = "data9"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "requested by eng team"
+    change_reason       = "To add existing account to AFT"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "thevjajer-IBG-PROD"
+}
